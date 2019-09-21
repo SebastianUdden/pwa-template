dbb35a3ff388c02c4f2b0f74debc34fb36134d07
Sebastian Uddén <sebastian.udden@danielwellington.com>
2019-09-21 14:32:26 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `PWA Template`,
+    description: `The Progressive Web App starter pack, go make awesome stuff with it.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -18,17 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Progressive Web App`,
+        short_name: `PWA`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#444`,
+        theme_color: `#222`,
+        display: `standalone`,
+        icon: `src/images/rocket.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { SVG } from "../components/svg/svg"
+import { rocket } from "../svgs/rocket.js"
+import { rocketColored } from "../svgs/rocketColored.js"
+
+const P = styled.p``
+
+const SVGWrapper = styled.div`
+  padding: 0;
+  margin: 0;
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hey buddy!</h1>
+    <P>
+      This will be the base of many awesome apps, enjoy your{" "}
+      <SVG {...rocket} size={18} color="#fff" /> below...
+    </P>
+    <SVGWrapper>
+      <SVG {...rocketColored} size={240} />
+    </SVGWrapper>
   </Layout>
 )
 
